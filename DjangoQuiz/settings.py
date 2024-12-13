5840a19df4570a808880758af28f4eac841deceb
Muhammedsadhath <muhammedsadhath10@gmail.com>
2024-12-13 17:32:11 +0530

    Initial commit for quiz app

--- DjangoQuiz/settings.py
+++ DjangoQuiz/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,8 +29,7 @@ SECRET_KEY = 'django-insecure-l7xlxu)s@@aka#+3oxbl^yr31*00--*o887$6%gl98$0frett2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']  #THIS IS FOR DEPLYMENT
 
 # Application definition
 
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #THIS IS FOR DEPLYMENT
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #THIS IS FOR DEPLYMENT
 
 ROOT_URLCONF = 'DjangoQuiz.urls'
 
@@ -77,11 +82,19 @@ WSGI_APPLICATION = 'DjangoQuiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD_YO'),
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '16792',
+    
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -117,8 +130,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS=[BASE_DIR/'static']
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[BASE_DIR/'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Add root-level static folder here
+    os.path.join(BASE_DIR, 'staticfiles'),  # If you're using another directory
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')  # Used for collectstatic
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
